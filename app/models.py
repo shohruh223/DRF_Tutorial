bfc882a88238d6bcc1167a06e4b10b69e768137d
shohruh223 <shohruh.abd2223@gmail.com>
2023-07-18 12:21:58 +0500

    add models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=180)
+
+    def __str__(self):
+        return self.title
+
+
+class Product(BaseModel):
+    image = models.ImageField(upload_to='product/')
+    title = models.CharField(max_length=150)
+    description = models.TextField(null=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+    category = models.ForeignKey(to='app.Category',
+                                 on_delete=models.CASCADE,
+                                 related_name='products')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table = 'product'
