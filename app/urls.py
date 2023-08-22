4ace5ec97c8f3a429cf4e1c8e22c284c108c3754
shohruh223 <shohruh.abd2223@gmail.com>
2023-08-22 14:44:00 +0500

    update

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
+from app.views import ProductListView, ProductCreateView, ProductEditView, ProductDeleteView, ProductGetView
 
-from app.views import ProductView
 
 urlpatterns = [
-    path('product/', ProductView.as_view(), name='product-list'),
-    path('product/<int:product_id>', ProductView.as_view(), name='product-detail'),
+    path('products/', ProductListView.as_view(), name='products'),
+    path('add-product/', ProductCreateView.as_view(), name='add-product'),
+    path('edit-product/<int:product_id>', ProductEditView.as_view(), name='edit-product'),
+    path('product/<int:product_id>', ProductGetView.as_view(), name='product'),
+    path('delete-product/<int:product_id>', ProductDeleteView.as_view(), name='delete-product'),
+
 ]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,5 @@
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -5,24 +7,32 @@ from app.models import Product
 from app.serializers import ProductModelSerializer
 
 
-class ProductView(APIView):
-    def get(self, request, product_id=None):
-        if product_id:
-            try:
-                product = Product.objects.filter(id=product_id).first()
-                serializer = ProductModelSerializer(product)
-                return Response(data=serializer.data,
-                                status=status.HTTP_200_OK)
-            except Product.DoesNotExist:
-                return Response(data={"errors":"This product is not found"},
-                                status=status.HTTP_404_NOT_FOUND)
-        else:
-            products = Product.objects.all()
-            serializer = ProductModelSerializer(instance=products,
-                                                many=True)
-            return Response(data=serializer.data,
-                            status=status.HTTP_200_OK)
+class ProductListView(APIView):
 
+    def get(self, request):
+        products = Product.objects.all()
+        serializer = ProductModelSerializer(instance=products, many=True)
+        return Response(data=serializer.data,
+                        status=status.HTTP_200_OK)
+
+
+class ProductCreateView(APIView):
+
+    @swagger_auto_schema(
+        request_body=openapi.Schema(
+            type="object",
+            properties={
+                "title": openapi.Schema(type="string"),
+                "description": openapi.Schema(type="string"),
+                "price": openapi.Schema(type="integer"),
+            },
+            required=["title", "price"]
+        ),
+        responses={
+            201: "Product created successfully",
+            400: "Invalid data provided"
+        }
+    )
     def post(self, request):
         serializer = ProductModelSerializer(data=request.data)
         if serializer.is_valid():
@@ -32,11 +42,45 @@ class ProductView(APIView):
         return Response(data=serializer.errors,
                         status=status.HTTP_400_BAD_REQUEST)
 
+
+class ProductGetView(APIView):
+
+    def get(self, request, product_id):
+        if product_id:
+            product = Product.objects.filter(id=product_id).first()
+            serializer = ProductModelSerializer(product)
+            return Response(data=serializer.data,
+                            status=status.HTTP_200_OK)
+        return Response(data={"errors":"This product is not found"},
+                        status=status.HTTP_404_NOT_FOUND)
+
+
+class ProductEditView(APIView):
+
+    @swagger_auto_schema(
+        manual_parameters=[
+            openapi.Parameter('product_id', openapi.IN_PATH, description="ID of the product to edit",
+                              type=openapi.TYPE_INTEGER),
+        ],
+        request_body=openapi.Schema(
+            type="object",
+            properties={
+                "title": openapi.Schema(type="string"),
+                "description": openapi.Schema(type="string"),
+                "price": openapi.Schema(type="integer"),
+            },
+        ),
+        responses={
+            200: "Product updated successfully",
+            400: "Invalid data provided",
+            404: "Product not found"
+        }
+    )
     def put(self, request, product_id):
         try:
             product = Product.objects.filter(id=product_id).first()
         except Product.DoesNotExist:
-            return Response(data={"errors":"This product is not found"},
+            return Response(data={"errors": "This product is not found"},
                             status=status.HTTP_404_NOT_FOUND)
 
         serializer = ProductModelSerializer(instance=product,
@@ -47,6 +91,9 @@ class ProductView(APIView):
                             status=status.HTTP_200_OK)
         return Response(data=serializer.errors,
                         status=status.HTTP_400_BAD_REQUEST)
+
+
+class ProductDeleteView(APIView):
 
     def delete(self, request, product_id):
         try:
