69634ae2f6a72e7c542598088d2259b7ae7e3329
shohruh223 <shohruh.abd2223@gmail.com>
2023-09-10 19:14:28 +0500

    change urls

--- root/urls.py
+++ root/urls.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from django.contrib.auth.views import LoginView
 from django.urls import path, include
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
@@ -19,9 +18,9 @@ schema_view = get_schema_view(
 )
 
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('', include('app.urls')),
     path('api-auth/', include('rest_framework.urls')),
 ]
